d74113da8adc1de2f52a12082ee90f010712a829
Nour Everaert <nour.everaert.prof@gmail.com>
2023-01-31 15:56:20 +0100

    envoi mail pas terminé

--- mail-template.php
+++ mail-template.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/mail.css">
+    <title>Contact</title>
 </head>
 
-<body style="display:flex;flex-direction:column; align-items:center; background-color: black; color:white">
-    <h1>Hello</h1>
-    <p>We have received your order, it will be delivered soon.</p>
-</body>
+<div>
+    <img src="./ressources/Hackers.png" alt="logo de Hackers Poulette">
+    <h1>Message well received!</h1>
+    <p>Dear customer, <br>
+
+        We have received your email and thank you for it. We will process it within 5 working days.
+        If necessary, depending on your request, we will contact you again. <br>
+
+        Thank you for your patience and understanding. <br>
+
+        Sincerely yours, <br>
+
+        The Service Center Team</p>
+</div>
+
 
 </html>
